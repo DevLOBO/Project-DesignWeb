d8efa1e9e0a860823e07366376b86d398f5dce30
DevLOBO <franco.hernandezh@my.unitec.edu.mx>
2018-05-16 21:02:46 -0500

    Sitio web

---
+++ Estilos/Buscar.php
@@ -0,0 +1,12 @@
+<script>
+  (function() {
+    var cx = '009558067700047006548:cqrdqyl_7kq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
