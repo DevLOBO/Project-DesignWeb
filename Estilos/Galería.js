955d23035bd90de60cdf84004fe50fc496949b6e
DevLOBO <franco.hernandezh@my.unitec.edu.mx>
2018-05-16 23:04:52 -0500

    Actualización

--- Estilos/Galería.js
+++ Estilos/Galería.js
@@ -1,19 +1,18 @@
-/** javascript slide-show **/
 var cons = 1;
 function slide_show(){
-	var elemento = document.getElementById('galeria').getElementsByTagName('li');	
-	for(var n=cons; n <= elemento.length; n++){		
+	var elemento = document.getElementById('galeria').getElementsByTagName('li');
+	for(var n=cons; n <= elemento.length; n++){
 		elemento[n].className = 'selected';
 		for(var i = 0; i<elemento.length; i++){
 			if(i!=cons){
 				elemento[i].className = 'noselected';
-			}		
+			}
 		}
-		cons++;							
+		cons++;
 		break;
-	}	
+	}
 	if(cons >= elemento.length){
-		cons = 0;		
+		cons = 0;
 	}
 	return false;
 }
